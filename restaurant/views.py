8e06dd1f06aa9560feb61accedacf251b2d42ff6
Ilya Kozak <kozak@tut.by>
2023-07-05 14:57:14 +0300

    feat: add menu, booking views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Menu, Booking
+from .serializers import MenuSerilializer, BookingSerilializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(APIView):
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = MenuSerilializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MenuSerilializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({ 'status': 'success', 'data': serializer.data })
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+class SingleMenuItemView(APIView):
+    def get(self, request, pk):
+        item = get_object_or_404(Menu, pk=pk)
+        serializer = MenuSerilializer(item)
+        return Response({ 'status': 'success', 'data': serializer.data })
+        
+        
+class BookingView(APIView):
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerilializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = BookingSerilializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({ 'status': 'success', 'data': serializer.data })
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
